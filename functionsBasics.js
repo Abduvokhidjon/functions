b964ec9fa002e40e3d863c6107542d1fbb9579b4
Abduvokhidjon <131991957+Abduvokhidjon@users.noreply.github.com>
2023-06-19 16:04:20 +0000

    Function Basics

---
+++ functionsBasics.js
@@ -0,0 +1,55 @@
+// const calcArea = function(radius){
+//     let area = 3.14 * radius ** 2;
+//    return area;
+// }
+
+// const area = calcArea(5);
+// console.log(area);
+
+// const calcArea = (radius) => {
+//     return 3.14 * radius ** 2;
+// }; 
+// const calcArea = radius => 3.14 * radius ** 2 // arrow function
+
+// const area = calcArea(5);
+// console.log('area is: ', area);
+
+// const greet = function(){
+//     return 'hello, world';
+// }
+
+// const greet = () => 'hello. world';
+// console.log(greet())
+
+// const bill = function(products, tax){
+//     let total = 0;
+//     for(let i = 0; i < products.length; i++){
+//         total += products[i] + products[i] * tax;
+//     }
+//     return total
+// }
+// console.log(bill([10,15,30], 0.2));
+
+// arrow function
+
+// const bill = (products, tax) => {
+//     let total = 0
+//     for(let i = 0; i < products.length; i++){
+//         total += products[i] + products[i] * tax;
+//     }
+//     return total
+// }
+// console.log(bill([10,15,30], 0.2));
+
+// functions
+
+
+const name = 'Ali';
+const greet = () => 'hello';
+let reultOne = greet();
+console.log(reultOne);
+
+// method 
+
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
